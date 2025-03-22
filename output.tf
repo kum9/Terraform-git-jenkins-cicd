6ea74bede1d66424e859e85cdc5a2159abd4bcf4
kum9 <etiennekum12@gmail.com>
2025-03-21 18:50:51 -0600

    Resolved merge conflict in main.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,9 @@
 output "public_ip" {
-  value       = aws_instance.web.public_ip
-  description = "The public IP address of the instance"
+  value = aws_instance.web[count.index].public_ip
 }
 
 output "instance_id" {
-  value       = aws_instance.web.id
-  description = "The ID of the instance"
+  value = aws_instance.web[count.index].id
 }
 
 output "security_group_id" {
