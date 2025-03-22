74227d6bdc08cd9c1aca233cf5b3168e91012ea6
kum9 <etiennekum12@gmail.com>
2025-03-21 18:45:16 -0600

    Resolved merge conflict in main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,50 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id        # Reference the valid AMI ID
-  count         = 2                 # Create 2 instances
-  instance_type = var.instance_type # Directly set the instance type
+  instance_type = var.instance_type # Directly set the instance type to t2.medium
+  security_groups = [aws_security_group.web.name]
 
-  vpc_security_group_ids = [aws_security_group.web.id] # Use security group ID instead of name
+  count = 3 # Create 3 instances
 
+  # Define root volume size (30 GB)
   root_block_device {
-    volume_size           = var.volume_size           # Set the volume size
-    volume_type           = var.volume_type           # General Purpose SSD
-    delete_on_termination = var.delete_on_termination # Ensure volume deletion on termination
+    volume_size           = var.volume_size           # Set the volume size to 30 GB
+    volume_type           = var.volume_type           # General Purpose SSD (can be changed to gp3 or other types)
+    delete_on_termination = var.delete_on_termination # Set to true so the volume will be deleted when the instance is terminated
   }
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld-${count.index}" # Unique name for each instance (e.g., HelloWorld-0, HelloWorld-1, HelloWorld-2)
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Web Security Group"
+
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
   }
 }
