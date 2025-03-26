2e312d0ef381c8e983d9fc5597a98e93d53f1ce9
kum9 <etiennekum12@gmail.com>
2025-03-25 19:31:37 -0600

    Update access and secret access keys on variables.tf file

--- provider.tf
+++ provider.tf
@@ -8,8 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  
+  region     = "us-west-1"  # Change to your correct region
 }
+  
